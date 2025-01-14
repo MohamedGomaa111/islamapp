3b0092a6843f045ed051a26d8634c2de2158be1e
mohamed gpmaa <mohamedgomaa01000824570@gmail.com>
2025-01-14 16:55:21 +0200

    add colors

--- lib/AppColors.dart
+++ lib/AppColors.dart
@@ -4,9 +4,7 @@ class Appcolors {
   static const Color primaryDark = Color(0xFFE2BE7F);
   static const Color blackColor = Color(0xff202020);
   static const Color blackBGNBColor = Color(0x99FFFFFF);
+  static const Color blackBGNBColor1 = Color(0x98FFFFFF);
 
   static const Color whiteColor = Color(0xffFFFFFF);
-  static const Color redcolor =
-      Color.from(alpha: 1, red: 0.957, green: 0.263, blue: 0.212);
-  static const Color plucolor = Colors.purple;
 }
