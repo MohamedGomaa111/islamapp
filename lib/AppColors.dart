7b36bb7d5f99505f8846fa5aa6dc6cfe25903d8d
Mohamed Gomaa  Zayed <mohamedgomaa01000824570@gmail.com>
2025-01-14 16:54:19 +0200

    add color

--- lib/AppColors.dart
+++ lib/AppColors.dart
@@ -4,6 +4,7 @@ class Appcolors {
   static const Color primaryDark = Color(0xFFE2BE7F);
   static const Color blackColor = Color(0xff202020);
   static const Color blackBGNBColor = Color(0x99FFFFFF);
+  static const Color blackBGNBColor1 = Color(0x98FFFFFF);
 
   static const Color whiteColor = Color(0xffFFFFFF);
 }
